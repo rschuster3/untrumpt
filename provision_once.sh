2411a9000adaebc78d0eb76a8dd52bb82bccea25
rschuster3 <rschuster@industrydive.com>
2016-11-30 12:51:46 -0500

    pg sql overwrite local auth method for non psql users

--- provision_once.sh
+++ provision_once.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y python-pip
 . ./app.env
 
 sudo apt-get install -y postgresql postgresql-contrib
+echo "cp /vagrant/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf" | sudo -i -u postgres
 echo "psql -c \"CREATE USER \"${DB_USER}\" WITH PASSWORD '${DB_PASSWORD}';\"" | sudo -i -u postgres
 echo "psql -c \"CREATE DATABASE ${DB_NAME};\"" | sudo -i -u postgres
 echo "psql -c \"GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};\"" | sudo -i -u postgres
