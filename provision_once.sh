ea5509a63567c173ca301900e1332deaefdbb27b
rschuster3 <rschuster@industrydive.com>
2016-11-30 14:17:10 -0500

    _

--- provision_once.sh
+++ provision_once.sh
@@ -5,7 +5,6 @@ sudo apt-get install -y python-pip
 . ./app.env
 
 sudo apt-get install -y postgresql postgresql-contrib
-echo "echo \"${DB_PASSWORD}\" | createuser ${DB_USER} -W" | sudo -i -u postgres
-echo "psql -c \"ALTER USER ${DB_USER} PASSWORD '${DB_PASSWORD}';\"" | sudo -i -u postgres
+echo "psql -c \"CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';\"" | sudo -i -u postgres
 echo "psql -c \"CREATE DATABASE ${DB_NAME};\"" | sudo -i -u postgres
 echo "psql -c \"GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};\"" | sudo -i -u postgres
